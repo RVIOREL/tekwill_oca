3dd204808c3b96eac9ec096ebf72054891306cd4
Rusu <viorel.projects@gmail.com>
2018-04-24 20:21:37 +0300

    lesson3

---
+++ lesson3/src/MyFirstPakage/MyFirstClass.java
@@ -0,0 +1,13 @@
+package MyFirstPakage;
+
+public class MyFirstClass {
+
+    public static void main(String[] args){
+        int total = suma(1, 2);
+        System.out.print("My first java application : "+total);
+    }
+
+    public static int suma(int x, int y){
+        return x + y;
+    }
+}
